dc73074881f42c7a3cdc726a9cc0b10c67d5eb3b
Vladislav <itochanenko@gmail.com>
2019-09-26 00:05:02 +0300

    Fixed an issue when processes wouldn't finish

--- Lab 1/src/main/java/Main.java
+++ Lab 1/src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("It works!");
-        Process p = Runtime.getRuntime().exec("java -jar module_f.jar 0 0");
+        Process p = Runtime.getRuntime().exec("java -jar module_f.jar 0 3");
+        p.waitFor();
         System.out.println(p.exitValue());
-        Process p2 = Runtime.getRuntime().exec("java -jar module_g.jar 0 0");
+        Process p2 = Runtime.getRuntime().exec("java -jar module_g.jar 0 3");
+        p2.waitFor();
         System.out.println(p2.exitValue());
     }
 }
